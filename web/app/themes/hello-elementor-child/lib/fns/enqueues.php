11d4596c52f6916ff100f3b004a0bf65f8587015
Michael Wender <michael@michaelwender.com>
2023-09-19 07:31:49 -0400

    Dequeuing jQuery Migrate

--- web/app/themes/hello-elementor-child/lib/fns/enqueues.php
+++ web/app/themes/hello-elementor-child/lib/fns/enqueues.php
@@ -18,3 +18,13 @@ function hello_elementor_child_scripts_styles() {
   ]);
 }
 add_action( 'wp_enqueue_scripts', 'hello_elementor_child_scripts_styles', 20 );
+
+function dequeue_jquery_migrate( $scripts ) {
+    if ( ! is_admin() && ! empty( $scripts->registered['jquery'] ) ) {
+        $scripts->registered['jquery']->deps = array_diff(
+            $scripts->registered['jquery']->deps,
+            [ 'jquery-migrate' ]
+        );
+    }
+}
+add_action( 'wp_default_scripts', 'dequeue_jquery_migrate' );
