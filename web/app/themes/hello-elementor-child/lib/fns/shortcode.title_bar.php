952f433a68534ccf3d63d122a5237daac763d022
Michael Wender <michael@michaelwender.com>
2023-09-26 10:36:12 -0400

    Initiallizing var

--- web/app/themes/hello-elementor-child/lib/fns/shortcode.title_bar.php
+++ web/app/themes/hello-elementor-child/lib/fns/shortcode.title_bar.php
@@ -3,6 +3,7 @@ namespace BizPlanner\shortcodes;
 
 function title_bar(){
   global $current_business_plan;
-  return '<style>.title-bar span{font-family: "Segoe UI",Roboto,Helvetica,Arial,sans-serif,"Apple Color Emoji","Segoe UI Emoji"; font-weight: 500;}</style><div class="title-bar">Currently editing: <span>' . $current_business_plan['title'] . '</span></div>';
+  $title = ( is_array( $current_business_plan ) && array_key_exists( 'title', $current_business_plan ) )? $current_business_plan['title'] : 'Business Plan #1';
+  return '<style>.title-bar span{font-family: "Segoe UI",Roboto,Helvetica,Arial,sans-serif,"Apple Color Emoji","Segoe UI Emoji"; font-weight: 500;}</style><div class="title-bar">Currently editing: <span>' . $title . '</span></div>';
 }
 add_shortcode( 'title_bar', __NAMESPACE__ . '\\title_bar' );
