575897b1b3c874688f8b9a552fc0cc6aa8b05fd7
Michael Wender <michael@michaelwender.com>
2023-09-28 08:59:10 -0400

    Setting up ACF Local JSON

--- web/app/themes/hello-elementor-child/lib/fns/acf.php
+++ web/app/themes/hello-elementor-child/lib/fns/acf.php
@@ -20,3 +20,35 @@ function load_select_values( $field ) {
   return $field;
 }
 add_filter( 'acf/load_field/name=sidebar_menu', __NAMESPACE__ . '\\load_select_values' );
+
+
+/**
+ * Saves ACF configuration as JSON.
+ *
+ * @param      string  $path   The path
+ *
+ * @return     string  Returns ACF JSON save location.
+ */
+function acf_json_save_point( $path ) {
+  return get_stylesheet_directory() . '/lib/acf-json';
+}
+add_filter('acf/settings/save_json', __NAMESPACE__ . '\\acf_json_save_point');
+
+/**
+ * Loads ACF configuration from JSON.
+ *
+ * @param      array  $paths  The paths
+ *
+ * @return     array  Array of ACF JSON locations.
+ */
+function acf_json_load_point( $paths ) {
+    // remove original path
+    unset($paths[0]);
+
+    // append path
+    $paths[] = get_stylesheet_directory() . '/lib/acf-json';
+
+    // return
+    return $paths;
+}
+add_filter('acf/settings/load_json', __NAMESPACE__ . '\\acf_json_load_point');
