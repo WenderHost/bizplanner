ce5d270f06006a7528bce5adc3da14a59bd36822
Michael Wender <michael@michaelwender.com>
2023-10-31 10:01:34 -0400

    Reorganizing shortcode logic file structure, Adding BP_DIR constant

--- web/app/themes/hello-elementor-child/functions.php
+++ web/app/themes/hello-elementor-child/functions.php
@@ -17,27 +17,23 @@ if ( ! defined( 'ABSPATH' ) ) {
 define( 'HELLO_ELEMENTOR_CHILD_VERSION', '2.0.0' );
 define( 'BP_REST_NAMESPACE', 'bizplanner/v1' );
 define( 'BP_BIZPLAN_ROUTE', '/bizplan/' );
+define( 'BP_DIR', trailingslashit( get_stylesheet_directory( __FILE__ ) ) );
 
 /**
  * Include required files
  */
-require_once( get_stylesheet_directory( __FILE__ ) . '/lib/fns/acf.php' );
-require_once( get_stylesheet_directory( __FILE__ ) . '/lib/fns/adminbar.php' );
-require_once( get_stylesheet_directory( __FILE__ ) . '/lib/fns/api.php' );
-require_once( get_stylesheet_directory( __FILE__ ) . '/lib/fns/cpt.question.php' );
-require_once( get_stylesheet_directory( __FILE__ ) . '/lib/fns/debugging.php' );
-require_once( get_stylesheet_directory( __FILE__ ) . '/lib/fns/enqueues.php' );
-require_once( get_stylesheet_directory( __FILE__ ) . '/lib/fns/navigation.php' );
-require_once( get_stylesheet_directory( __FILE__ ) . '/lib/fns/shortcode.next-prev-navigation.php' );
-require_once( get_stylesheet_directory( __FILE__ ) . '/lib/fns/shortcode.page_numbers.php' );
-require_once( get_stylesheet_directory( __FILE__ ) . '/lib/fns/shortcode.post_id.php' );
-require_once( get_stylesheet_directory( __FILE__ ) . '/lib/fns/shortcode.new_business_plan_button.php' );
-require_once( get_stylesheet_directory( __FILE__ ) . '/lib/fns/shortcode.question_form.php' );
-require_once( get_stylesheet_directory( __FILE__ ) . '/lib/fns/shortcode.question_sidebar_nav.php' );
-require_once( get_stylesheet_directory( __FILE__ ) . '/lib/fns/shortcode.title_bar.php' );
-require_once( get_stylesheet_directory( __FILE__ ) . '/lib/fns/templates.php' );
-require_once( get_stylesheet_directory( __FILE__ ) . '/lib/fns/users.php' );
-require_once( get_stylesheet_directory( __FILE__ ) . '/lib/fns/utilities.php' );
+require_once( BP_DIR . 'lib/fns/acf.php' );
+require_once( BP_DIR . 'lib/fns/adminbar.php' );
+require_once( BP_DIR . 'lib/fns/api.php' );
+require_once( BP_DIR . 'lib/fns/cpt.question.php' );
+require_once( BP_DIR . 'lib/fns/debugging.php' );
+require_once( BP_DIR . 'lib/fns/enqueues.php' );
+require_once( BP_DIR . 'lib/fns/makepdf.php' );
+require_once( BP_DIR . 'lib/fns/navigation.php' );
+require_once( BP_DIR . 'lib/fns/shortcodes.php' );
+require_once( BP_DIR . 'lib/fns/templates.php' );
+require_once( BP_DIR . 'lib/fns/users.php' );
+require_once( BP_DIR . 'lib/fns/utilities.php' );
 
 if( ! is_admin() ){
   $current_business_plan = BizPlanner\users\get_current_business_plan();
---
+++ web/app/themes/hello-elementor-child/lib/fns/shortcodes.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Shortcode includes for logic found in /lib/fns/shortcodes/
+ */
+require_once( BP_DIR . 'lib/fns/shortcodes/bpdata.php' );
+//require_once( BP_DIR . 'lib/fns/shortcodes/makepdf.php' );
+require_once( BP_DIR . 'lib/fns/shortcodes/new_business_plan_button.php' );
+require_once( BP_DIR . 'lib/fns/shortcodes/next-prev-navigation.php' );
+require_once( BP_DIR . 'lib/fns/shortcodes/page_numbers.php' );
+require_once( BP_DIR . 'lib/fns/shortcodes/post_id.php' );
+require_once( BP_DIR . 'lib/fns/shortcodes/question_form.php' );
+require_once( BP_DIR . 'lib/fns/shortcodes/question_sidebar_nav.php' );
+require_once( BP_DIR . 'lib/fns/shortcodes/title_bar.php' );
