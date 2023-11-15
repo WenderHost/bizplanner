38c653f3f152540974854b434cd2706df66f653b
Michael Wender <michael@michaelwender.com>
2023-11-15 13:50:39 -0500

    Updating icon, loading from template partial

--- web/app/themes/bizplanner/lib/partials/layout-header.php
+++ web/app/themes/bizplanner/lib/partials/layout-header.php
@@ -8,7 +8,7 @@ $avatar_url = ( $current_business_plan )? BP_DIR_URI . 'lib/img/bizplanner-avata
 
     <div class="d-flex align-items-center justify-content-between">
       <a href="<?= home_url() ?>" class="logo d-flex align-items-center">
-        <svg width="32" id="Layer_1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256"><style>.st0{fill:#085b8a}.st1{fill:#0b7fc1}.st2{fill:#fff}</style><path class="st0" d="M211 223.7c0 5.5-4.5 10-10 10H67.4c-5.5 0-10-4.5-10-10V44.9c0-5.5 4.5-10 10-10H201c5.5 0 10 4.5 10 10v178.8z"/><path class="st1" d="M198.6 211.1c0 5.5-4.5 10-10 10H55c-5.5 0-10-4.5-10-10V32.3c0-5.5 4.5-10 10-10h133.6c5.5 0 10 4.5 10 10v178.8z"/><path class="st2" d="M181.8 52.4c0 4.1-3.6 7.5-8 7.5h-104c-4.4 0-8-3.4-8-7.5s3.6-7.5 8-7.5h104c4.4 0 8 3.3 8 7.5zM170.6 86.4c0 4.1-3.6 7.5-8 7.5H69.8c-4.4 0-8-3.4-8-7.5s3.6-7.5 8-7.5h92.8c4.4 0 8 3.4 8 7.5zM158 120.5c0 4.1-3.6 7.5-8 7.5H69.8c-4.4 0-8-3.4-8-7.5s3.6-7.5 8-7.5H150c4.4 0 8 3.3 8 7.5zM158 154.5c0 4.1-3.6 7.5-8 7.5H69.8c-4.4 0-8-3.4-8-7.5s3.6-7.5 8-7.5H150c4.4 0 8 3.4 8 7.5zM134.7 188.6c0 4.1-3.6 7.5-8 7.5h-57c-4.4 0-8-3.4-8-7.5s3.6-7.5 8-7.5h57c4.4 0 8 3.3 8 7.5z"/></svg>
+        <?php get_template_part( 'lib/partials/bizplanner', 'icon' ) ?>
         <span class="d-none d-lg-block">BizPlanner</span>
       </a>
       <i class="bi bi-list toggle-sidebar-btn"></i>
