fa7a22608bf3a701c967fd3343baa622d9e68e9a
Michael Wender <michael@michaelwender.com>
2023-11-21 13:54:00 -0500

    Directly calling function in lieu of shortcode

--- web/app/themes/bizplanner/lib/partials/layout-sidebar.php
+++ web/app/themes/bizplanner/lib/partials/layout-sidebar.php
@@ -1,7 +1,7 @@
 <aside id="sidebar" class="sidebar d-print-none" style="<?php if( is_admin_bar_showing() ){ echo 'margin-top: 26px;';} ?>">
   <?php
   if( is_user_logged_in() && is_single() && 'question' == get_post_type() ):
-  echo do_shortcode( '[question_sidebar_nav]' );
+    echo BizPlanner\shortcodes\get_question_sidebar_nav([]);
   endif;
   ?>
 </aside><!-- End Sidebar-->
