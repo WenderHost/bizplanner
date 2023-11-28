9aee28413b7473510c7677b680148cd795f72493
Michael Wender <michael@michaelwender.com>
2023-11-28 14:58:51 -0500

    Namespacing, removing references to Hello Elementor

--- web/app/themes/bizplanner/lib/fns/enqueues.php
+++ web/app/themes/bizplanner/lib/fns/enqueues.php
@@ -1,4 +1,5 @@
 <?php
+namespace BizPlanner\enqueues;
 use function BizPlanner\question\{get_adjacent_question};
 
 /**
@@ -6,10 +7,7 @@ use function BizPlanner\question\{get_adjacent_question};
  *
  * @return void
  */
-function hello_elementor_child_scripts_styles() {
-
-  wp_enqueue_style( 'hello-elementor-child-style', get_stylesheet_directory_uri() . '/style.css', [ 'hello-elementor-theme-style' ], HELLO_ELEMENTOR_CHILD_VERSION );
-
+function enqueue_scripts_styles() {
   $main_css = ( 'development' != BP_ENV )? 'dist/main.css' : 'main.css' ;
   wp_enqueue_style( 'bizplanner', get_stylesheet_directory_uri() . '/lib/css/' . $main_css, null, filemtime( get_stylesheet_directory() . '/lib/css/' . $main_css ) );
 
@@ -37,7 +35,7 @@ function hello_elementor_child_scripts_styles() {
     'next_question_url'     => $next_question_url,
   ]);
 }
-add_action( 'wp_enqueue_scripts', 'hello_elementor_child_scripts_styles', 20 );
+add_action( 'wp_enqueue_scripts', __NAMESPACE__ . '\\enqueue_scripts_styles', 20 );
 
 function dequeue_jquery_migrate( $scripts ) {
     if ( ! is_admin() && ! empty( $scripts->registered['jquery'] ) ) {
@@ -47,4 +45,4 @@ function dequeue_jquery_migrate( $scripts ) {
         );
     }
 }
-add_action( 'wp_default_scripts', 'dequeue_jquery_migrate' );
+add_action( 'wp_default_scripts', __NAMESPACE__ . '\\dequeue_jquery_migrate' );
